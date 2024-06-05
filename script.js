08514be8112d463a5586cef50fb3bed41bd73508
JeffVanCon <j.vanegaswork@gmail.com>
2024-06-04 18:29:15 -0600

    Calculator Base and Addition Funciton added

---
+++ script.js
@@ -0,0 +1,48 @@
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+
+function appendToDisplay(value) {
+  if (!operator) {
+    operand1 += value;
+  } else {
+    operand2 += value;
+  }
+
+  switch (value) {
+    case "+":
+      setOperator(value);
+      break;
+    default:
+      document.getElementById("display").value += value;
+  }
+}
+
+function setOperator(op) {
+  if (operand1) {
+    operator = op;
+    document.getElementById("display").value += " " + op + " ";
+  }
+}
+
+function calculate() {
+  let result;
+  switch (operator) {
+    case "+":
+      result = parseFloat(operand1) + parseFloat(operand2);
+      break;
+    default:
+      result = "";
+  }
+  document.getElementById("display").value = result;
+  operand1 = result.toString();
+  operand2 = "";
+  operator = "";
+}
+
+function clearDisplay() {
+  operand1 = "";
+  operand2 = "";
+  operator = "";
+  document.getElementById("display").value = "";
+}
